97d0d3f8e154e01a2eaca69bc0a81f3798223b62
Naofumi Okamoto <nokamoto.engr@gmail.com>
2020-09-08 13:53:52 +0900

    Add merge mode (#3)

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 all:
 	go fmt ./...
+
+	docker run \
+		-e GIT_BRANCH=development \
+		-e GIT_SHA=master \
+		-e DRY_RUN=1 \
+		--rm $$(docker build -q .)
+
 	docker run \
 		-e VALUES=iam.tag=foo,iam.image=bar \
 		-e VALUES_FILE=apps/values.yaml \
--- plugin.sh
+++ plugin.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #
 # GH_PAT: a github personal access token
+# DRY_RUN: set if run without remote changes
+#
+# tag rewrite mode:
 # VALUES: a key value list (e.g. foo=bar,baz=qux)
 # VALUES_FILE: a file path of helm values.yaml
-# DRY_RUN: set if run without remote changes
 # GIT_MESSAGE: a commit message
+#
+# merge mode:
+# GIT_BRANCH: a target git branch
+# GIT_SHA: a merged commit
 
 set -ex
 
@@ -12,15 +18,28 @@ git clone https://${GH_PAT}@github.com/nokamoto/demo20-apps-config.git
 
 cd demo20-apps-config
 
-app
+git config --local user.email "nokamoto.engr@gmail.com"
+git config --local user.name "nokamoto"
 
-git diff
-
-if [ -z "${DRY_RUN}" ]
+if [ -n "${GIT_BRANCH}" ]
 then
-    git config --local user.email "nokamoto.engr@gmail.com"
-    git config --local user.name "nokamoto"
-    git add .
-    git commit -m "${GIT_MESSAGE}"
-    git push origin master
+    git fetch
+    git checkout $GIT_BRANCH
+    git merge $GIT_SHA
+
+    if [ -z "${DRY_RUN}" ]
+    then
+        git push origin $GIT_BRANCH
+    fi
+else
+    app
+
+    git diff
+
+    if [ -z "${DRY_RUN}" ]
+    then
+        git add .
+        git commit -m "${GIT_MESSAGE}"
+        git push origin master
+    fi
 fi
